6a081c13a47ed8647df30f7196d53a3d0cc197a1
Aad Versteden <madnificent@gmail.com>
2017-04-07 15:35:45 +0200

    Upgrade application to latest ember and materialize
    The application was upgraded as we experienced issues with the
    previous build.  Our guess is that the breaks occurred due to
    libraries using unpublished internals or introducing breaking changes
    on minor versions.  We have upgraded both emberjs to the
    latest (1.12.0) and materializecss (0.21.0) and successfully rebuilt
    the application.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,7 +1,6 @@
-/*jshint node:true*/
-/* global require, module */
-var EmberApp = require('ember-cli/lib/broccoli/ember-app');
-var nodeSass = require('node-sass'); // loads the version in your package.json
+/* eslint-env node */
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const nodeSass = require('node-sass'); // loads the version in your package.json
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
