d45a0a4639afb5705a5751d99886457a71d5aee5
Nóra Szepes <nora.szepes@tenforce.com>
2017-09-01 14:54:12 +0200

    Added sliding from right to left for iframes

--- app/application/controller.js
+++ app/application/controller.js
@@ -8,10 +8,27 @@ const ApplicationController = Ember.Controller.extend({
   activeUi: Ember.computed('model.[]', 'activeId', function() {
     return this.get('model').findBy('id', this.get('activeId'));
   }),
+
+  previousIndex: -1,
+  directionClass: 'left',
+
   actions: {
-    selectUi(ui) {
+    selectUi(ui, index) {
+      if (this.get('activeId') == ui.get('id')) {
+        return;
+      }
+
+      var direction = 'left';
+      if (this.get('previousIndex') >= index) {
+        direction = "right";
+      }
+
+      this.set('directionClass', direction);
+      this.set('previousIndex', index);
+
       this.set('appendPath', ui.get('appendPath'));
       this.set('activeId', ui.get('id'));
+
     }
   }
 });
--- app/components/render-ui/component.js
+++ app/components/render-ui/component.js
@@ -1,15 +1,17 @@
 import Ember from 'ember';
 
 const RenderUiComponent = Ember.Component.extend({
-  classNameBindings: ['classes', 'isHidden:hidden'],
-  classes: 'iframe-view',
+  classNameBindings: ['mainClass', 'classes', 'isHidden:hidden', 'isActive:active'],
+  mainClass: 'iframe-view',
   frameUrl: Ember.computed('ui', function() {
     return this.get('ui.baseUrl') + this.get('ui.appendPath');
   }),
 
   isHidden: Ember.computed('ui', 'activeUi', function(){
     return !(this.get('ui') === this.get('activeUi'));
-  })
+  }),
+
+  isActive: Ember.computed.not('isHidden')
 });
 
 export default RenderUiComponent;
